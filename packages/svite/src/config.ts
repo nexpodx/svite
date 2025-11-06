8db4c4ffd17d2399ba8e1b22f8a99dcafb5ab843
Samuel <1787410363@qq.com>
2025-11-06 23:46:36 +0800

    feat：add js import handler and process config

---
+++ packages/svite/src/config.ts
@@ -0,0 +1,4 @@
+// config about
+
+// 合并配置文件
+export function resolveConfig(config: Object) {}
--- packages/svite/src/server/index.ts
+++ packages/svite/src/server/index.ts
@@ -1,25 +1,38 @@
-import { createServer } from 'node:http'
-import connect from 'connect'
-import { htmlHandler } from '../middlewares/htmlHandler'
+import { createServer } from "node:http";
+import connect from "connect";
+import { htmlHandler } from "../middlewares/htmlHandler";
+import { resolve } from "node:path";
+import { readFileSync } from "node:fs";
+import { resolveConfig } from "../config";
 
+export function createDevServer(inlineCconfig: Object) {
+  const config = resolveConfig(inlineCconfig);
 
-export function createDevServer(config: Object) {
-  const app = connect()
+  const app = connect();
 
-  let server = createServer(app)
+  let server = createServer(app);
 
   app.use((req, res, next) => {
-    if (req.url?.includes('.js')) {
-      res.writeHead(200, { 'Content-Type': 'application/javascript' });
+    if (req.url?.endsWith(".js")) {
+      res.writeHead(200, { "Content-Type": "application/javascript" });
+      // 构建文件路径并读取文件内容
+      const filePath = resolve(process.cwd(), req.url.substring(1)); // 去掉开头的 '/'
+      try {
+        const fileContent = readFileSync(filePath, "utf-8");
+        res.end(fileContent);
+      } catch (error) {
+        next(); // 如果文件不存在，继续下一个中间件
+      }
+    } else {
+      next();
     }
-    next()
-  })
+  });
 
-  app.use(htmlHandler)
+  app.use(htmlHandler);
 
   server.listen(3000, () => {
-    console.log('Dev server running on http://localhost:3000')
-  })
+    console.log("Dev server running on http://localhost:3000");
+  });
 
-  console.log('server created', config);
+  console.log("server created", config);
 }
